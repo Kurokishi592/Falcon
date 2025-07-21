7a4bf6c0570d7b5923cb8cfca41f1cc6de42b284
joeLku1 <joelku77@gmail.com>
2025-07-21 21:48:34 +0800

    added some new tests in line with advisor's comments

--- python_tests.py
+++ python_tests.py
@@ -10,7 +10,7 @@ class TestFalcon(unittest.TestCase):
     Automatic Unit Tests for Falcon
     """
 
-    def test_apriltag_3_detection(self):
+    def test_apriltag_recursive_3_detection(self):
         """
         Tests if AprilTag detector detects the tags correctly
         Checks for number of tags detected, and IDs
@@ -20,13 +20,59 @@ class TestFalcon(unittest.TestCase):
             detector = AprilTagDetector()
             image = cv2.imread('Tests/test_3_recursive.png')
             tags = detector.detect(image)
-            print(tags[0])
             self.assertEqual(len(tags), 3, "Expected to detect three tags in the test image")
             self.assertEqual(tags[0].tag_id, 0, "Expected first tag ID to be 0")
             self.assertEqual(tags[1].tag_id, 1, "Expected second tag ID to be 1")
             self.assertEqual(tags[2].tag_id, 2, "Expected third tag ID to be 2")
         except Exception as e:
-            self.fail(f"AprilTag detection failed {e}")
+            self.fail(f"AprilTag recursive detection failed {e}")
+
+    
+    def test_apriltag_no_detection(self):
+        """
+        Tests if AprilTag detector detects no tags in a blank image
+        Should detect zero tags
+        """
+        try:
+            detector = AprilTagDetector()
+            image = cv2.imread('Tests/test_0_blank.png')
+            tags = detector.detect(image)
+            self.assertEqual(len(tags), 0, "Expected to detect zero tags in the test image")
+        except Exception as e:
+            self.fail(f"AprilTag no detection failed {e}")
+
+
+    def test_apriltag_rotate_detection(self):
+        """
+        Tests if AprilTag detector detects tags in a rotated tag
+        Should detect two tags with IDs 4, 5 and 6
+        """
+        try:
+            detector = AprilTagDetector()
+            image = cv2.imread('Tests/test_3_rotated.png')
+            tags = detector.detect(image)
+            self.assertEqual(len(tags), 2, "Expected to detect two tags in the test image")
+            self.assertEqual(tags[0].tag_id, 4, "Expected first tag ID to be 4")
+            self.assertEqual(tags[1].tag_id, 5, "Expected second tag ID to be 5")
+        except Exception as e:
+            self.fail(f"AprilTag rotation detection failed {e}")
+
+
+    def test_apriltag_warp_detection(self):
+        """
+        Tests if AprilTag detector detects tags in a warped tag
+        Should detect two tags with IDs 0 and 1 (image has 3 but due to warping, only 2 should be detected)
+        """
+        try:
+            detector = AprilTagDetector()
+            image = cv2.imread('Tests/test_3_warped.png')
+            tags = detector.detect(image)
+            self.assertEqual(len(tags), 2, "Expected to detect two tags in the test image")
+            self.assertEqual(tags[0].tag_id, 0, "Expected first tag ID to be 0")
+            self.assertEqual(tags[1].tag_id, 1, "Expected second tag ID to be 1")
+        except Exception as e:
+            self.fail(f"AprilTag warp detection failed {e}")
+
 
     def test_velocity(self):
         """
@@ -34,8 +80,8 @@ class TestFalcon(unittest.TestCase):
         Should return 0.03, 0.0, 0.0 for first and the negation for second
         """
         try:
-            frame1 = cv2.imread("Tests/original.png")
-            frame2 = cv2.imread("Tests/shifted.png")
+            frame1 = cv2.imread("Tests/test_3_original.png")
+            frame2 = cv2.imread("Tests/test_3_shifted.png")
             detector = AprilTagDetector()
             detection1 = detector.detect(frame1)
             detection2 = detector.detect(frame2)
@@ -54,7 +100,7 @@ class TestFalcon(unittest.TestCase):
             self.assertEqual(float(round(velocity2[1], 2)), 0.0, "Expected y velocity to be 0.0")
             self.assertEqual(float(round(velocity2[2], 2)), 0.0, "Expected z velocity to be 0.0")
         except Exception as e:
-            self.fail(f"Velocity computation failed {e}")
+            self.fail(f"AprilTag Velocity computation failed {e}")
 
 
 if __name__ == '__main__':
